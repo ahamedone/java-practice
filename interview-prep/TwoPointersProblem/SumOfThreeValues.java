09f95be015c0625af30941503320819f930edd41
Azhar Ahamed Sahib <azhar.a@msn.com>
2025-03-07 06:31:47 -0500

    Completed the SumOfThreeValues

--- interview-prep/TwoPointersProblem/SumOfThreeValues.java
+++ interview-prep/TwoPointersProblem/SumOfThreeValues.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 class SumOfThreeValues {
     
     public static void main(String[] param){
@@ -21,15 +25,51 @@ class SumOfThreeValues {
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
+        // sort the arrays
         Arrays.sort(nums);
+
+        // result List
         List<List<Integer>> result = new ArrayList<>();
 
         int n = nums.length;
-        for (int pivot = 0; pivot < n - 2; pivot++) {
-            if(nums[pivot] > 0){
+        for (int first = 0; first < n - 2; first++) {
+
+            // If an element is greater than 0, which means all the elements to the right too will be greater than the 1 and we can stop here.
+            if(nums[first] > 0){
                 break;
             }
-            
+
+            // If the current element and previous element are same, then we have a chance of getting the duplicate triplets, hence we skip the duplicate adjacent firsts
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+
+            int second = first+1;
+            int third = n-1;
+
+            while(second < third){
+                int total = nums[first] + nums[second] + nums[third];
+                if(total < 0){
+                    // This means sum is not enough, so we increment the second element to a higher value, in the sorted array.
+                    second++;
+                } else if(total > 0){
+                    // This means sum is more than zero, so we decrement the third element the next lower value, in the sorted array.
+                    third--;
+                } else {
+                    // means sum is 0, add to the result set
+                    result.add(Arrays.asList(nums[first], nums[second], nums[third]));
+
+                    // shrink the array, to avoid the duplicate triplets.
+                    second++; third--;
+                    while (second < third && nums[second] == nums[second - 1]) {
+                        second++;
+                    }
+                    while (second < third && nums[third] == nums[third + 1]) {
+                        third--;
+                    }
+                }
+            }  
         }
+        return result;
      }
 }
