7df274d1e81edf2cd12e921a43cdb9113d2fd661
Azhar Ahamed Sahib <azhar.a@msn.com>
2025-05-16 06:48:57 -0400

    New Java methods

---
+++ data-structures/AnonymousInterface.java
@@ -0,0 +1,88 @@
+import java.util.function.Predicate;
+import java.util.function.BiPredicate;
+import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+import java.util.List;
+import java.util.ArrayList;
+
+class AnonymousInterface {
+	public static void main(String[] args){
+		I lambdaInstance = () -> System.out.println("Lambda");
+		Predicate<String> predicate = s -> s.contains("City");
+		BiPredicate<String, String> biPredicate = (x, y) -> x != null && x.equals(y);
+		System.out.println("Predicate : " + predicate.test("Vatican City"));
+		System.out.println("BiPredicate : " + biPredicate.test("a" , "a"));
+
+		Supplier<String> supplier = () -> {return "Learning Java Anonymoud Class";};
+		System.out.println(supplier.get());
+
+
+		Consumer<String> consumer = (a) -> { System.out.println("Consumer Output : " + a); };
+		consumer.accept("Azhar");
+		lambdaInstance.m();
+
+		List<String> map = new ArrayList<String>();
+		map.add("Azhar 1"); map.add("Azhar 2");
+		map.forEach(System.out::println);
+
+		Function<String, Integer> function = x -> x.length();
+		System.out.println("Function : " + function.apply("India"));
+
+		BiFunction<String, String, String> biFunction = (x, y) -> x.concat(y);
+		System.out.println("BiFunction : " + biFunction.apply("India ", "Bharath"));
+
+		UnaryOperator<String> unaryOperator = x -> x.toUpperCase();
+		System.out.println("Uniary Operator : " + unaryOperator.apply("Azhar"));
+
+		BinaryOperator<String> binaryOperator = (x, y) -> x.concat(y);
+		System.out.println("BiFunction : " + binaryOperator.apply("India ", "Bharath"));
+
+		String testing = "Azhar";
+		BiFunction<String, String, Boolean> testing1 = String::equals;
+
+		System.out.println("Testing after the method reference : " + testing1.apply("Azhar", null));
+
+		Supplier<Integer> supplierInt = Person::howMany;
+		Function<Person, Integer> functionInt = Person::howMany;
+		BiFunction<Person, Person, Integer> biFunctionInteger = Person::howMany;
+
+		System.out.println(" supplierInt : " + supplierInt.get());
+		System.out.println(" functionInt : " + functionInt.apply(new Person()));
+		System.out.println(" biFunctionInteger : " + biFunctionInteger.apply(new Person(), new Person()));
+
+		Stream<String> names = Stream.of("Joe", "Mark", "Issac", "Alan", "Randy", "Peter");
+		Map<Integer, List<String>> lengthNameMaps = names
+														.collect(Collectors.groupingBy(String::length));
+		System.out.println(lengthNameMaps);
+
+
+
+	}
+}
+
+interface I{
+	void m();
+}
+
+class Person {
+	public static Integer howMany(Person people){
+		return 1;
+	}
+
+	public static Integer howMany(Person people1, Person people2){
+		return 2;
+	}
+
+	public static Integer howMany(){
+		return 0;
+	}
+
+
+}
--- data-structures/JavaStreams.java
+++ data-structures/JavaStreams.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class JavaStreams {
+    public static void main(String[] args){
+        List<Double> list = Arrays.asList(98d, 74d, 23d, 129d, 100d);
+        long count = list.stream().peek(System.out::println).filter(x -> x > 0).count();
+        System.out.println("Total Count : " + count);
+
+        Stream<Double> stream = Stream
+                                    .generate(()-> { return Math.random() * 100; })
+                                    .peek(System.out::println)
+                                    .limit(10);
+        stream.forEach(System.out::println);
+
+        Stream<String> strStream = Stream.of("Car", "Bus", "Train", "Aeroplane");
+        int length = strStream
+                        .reduce(0, (n, str)-> n+str.length(), Integer::sum);
+        System.out.println(length);
+
+        String name = Stream.of("s", "t", "r", "e", "a", "m")
+                        .reduce("", String::concat);
+        System.out.println(name);
+
+        Stream.of("s", "t", "r", "e", "a", "m","o").reduce(String::concat).ifPresent(System.out::println);
+
+        List<String> numbers = Arrays.asList("Azhar", "Jasmin", "Fatima", "Azeem");
+        String sum = numbers.parallelStream().reduce("a", (x, y) -> y.concat("-"), String::concat) ;
+        System.out.println(sum);
+    }
 }
