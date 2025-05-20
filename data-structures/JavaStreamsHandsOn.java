4b3f1f20639fcbdef0dedecfa1bc988fd5f5b1f8
azhar <azhar.techieday@gmail.com>
2025-05-20 06:57:23 -0400

    Java Streams HandsOn

--- data-structures/JavaStreamsHandsOn.java
+++ data-structures/JavaStreamsHandsOn.java
@@ -36,6 +36,33 @@ public class JavaStreamsHandsOn {
         Stream<String> flattenStream = stream.filter(x -> x.contains("c")).flatMap(Collection::stream);
         flattenStream.forEach(System.out::print);
 
+        /*
+        There are several parts to this:
+        a. Using 1, 2 and 3 create a List of Integers.
+            i. Stream the list and calculate the sum, using the sum() method from IntStream.
+            ii. Stream the list again and calculate the maximum value, using the max() method from IntStream.
+        b. Given the Person class (in the zip file), declare a List typed for Person with the following Person’s:
+            i. “Alan”, “Burke”, 22
+            ii. “Zoe”, “Peters”, 20
+            iii. “Peter”, “Castle”, 29
+           Using the max(Comparator) from Stream, calculate the oldest person in the list.
+        c. Using 10, 47, 33 and 23 create a List of Integers. Stream the list and using the following versions of reduce(), calculate the maximum value:
+            i. Optional<T> reduce(BinaryOperator<T> accumulator)
+            ii. T reduce(T identity, BinaryOperator<T> accumulator)
+         */
+
+        System.out.println("\n" + "*".repeat(20));
+        List<Integer> listOfIntegers = List.of(1, 2, 3);
+        int sum = listOfIntegers.stream().mapToInt(x -> x).sum();
+        System.out.println("Sum : " + sum);
+        int max = listOfIntegers.stream().mapToInt(x -> x).max().orElse(0);
+        System.out.println("Max : " + max);
+
+        List<Person1> person1List = List.of(new Person1("Alan", "Burke", 22),
+                                            new Person1("Zoe", "Peters", 20),
+                                            new Person1("Peter", "Castle", 29));
+        Person1 oldestPerson = person1List.stream().max(Person1::compareTo).orElse(null);
+        System.out.println("Oldest person : " + oldestPerson);
     }
 }
 
@@ -56,3 +83,26 @@ class Item implements Comparable<Item> {
 
     public int compareToId(Item o) {return id - o.id;}
 }
+
+class Person1 implements Comparable<Person1> {
+    String firstName;
+    String lastName;
+    int age;
+
+    public Person1(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person1 o) {
+        return age - o.age;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " " + age;
+    }
+}
+
