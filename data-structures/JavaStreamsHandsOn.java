fd4452892deaf85383da9102a4eb6156bd384d92
azhar <azhar.techieday@gmail.com>
2025-05-20 06:32:49 -0400

    Java Streams HandsOn

--- data-structures/JavaStreamsHandsOn.java
+++ data-structures/JavaStreamsHandsOn.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class JavaStreamsHandsOn {
     public static void main(String[] args) {
@@ -20,6 +23,19 @@ public class JavaStreamsHandsOn {
 
         List<Item> items = List.of(new Item(1, "Screw"), new Item(2, "Nail"), new Item(3, "Bolt"));
         items.stream().sorted(Item::compareTo).forEach(System.out::print);
+
+        /*
+        Generate a Stream<List<String>> using the Stream.of(Arrays.asList(“a”, “b”), Arrays.asList(“a”,
+        “c”)) method call. Filter the stream so that only list’s that contain “c” make it through the filter.
+        Flatten the Stream<List<String>> to a Stream<String> using the flatMap() operation. Note that
+        flapMap() states in the API “Each mapped stream is closed after its contents have been placed into
+        this [new] stream.”. Use forEach() to output the new stream.
+        */
+        System.out.println("\n" + "*".repeat(20));
+        Stream<List<String>> stream = Stream.of(Arrays.asList("a", "b"), Arrays.asList("a", "c"));
+        Stream<String> flattenStream = stream.filter(x -> x.contains("c")).flatMap(Collection::stream);
+        flattenStream.forEach(System.out::print);
+
     }
 }
 
